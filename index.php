26ad64392870fba0cc17d02985804a7162a1b88b
Ferdroid8080 <andresf@ferudinato.com>
2021-02-13 14:12:05 -0500

    wip: setup entrypoint file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+define('ROOT', dirname(__FILE__));
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Moonwalk\Core\Router;
+
+// Init Router
+$router = new Router();
+$request_uri = $router->checkUri( $_SERVER['REQUEST_URI'] );
+$router->checkController($request_uri[0], $request_uri[1], $request_uri[2]);
